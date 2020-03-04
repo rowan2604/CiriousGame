591258cfcf5d28d4437594640b4f399a741f483c
Nicolas Boizard <nicolas.boizard@isen.yncrea.fr>
2020-03-05 00:30:34 +0100

    Map update
    Collision / sprite behind wall etc...

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var game = new Phaser.Game(640, 495, Phaser.AUTO, '', { preload: preload, create: create, update: update/*, render: render*/});
-// 640x495
+
 function preload() {
     game.load.tilemap('map', 'map/map.json', null, Phaser.Tilemap.TILED_JSON); //Load map.json / Nicolas
-    game.load.image('tiles', 'map/tileset_Interior.png');//Load tileset.png / Nicolas
+    game.load.image('tiles', 'map/tileset_Interior.png'); //Load tileset.png / Nicolas
     game.load.spritesheet("zelda", "player/assets/zelda.png", 120, 130, 80) //Load character spritesheet / Antoine
-    game.load.image('statusBar', 'HUD/assets/StatusBar.png');  //Load statusBar image / P-T
+    game.load.image('statusBar', 'HUD/assets/StatusBar.png'); //Load statusBar image / P-T
     game.load.image('dropOfWater', 'HUD/assets/water.png'); //Load water drop image / P-T
 }
 
@@ -20,25 +20,24 @@ function create() {
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL; //Fullscreen mod just for test / Nicolas
     game.stage.backgroundColor = '#000000';
 
-    game.input.mouse.capture = true; //Capture mouse just for test can be remove / Nicolas
-    game.input.onDown.add(gofull, this); //Just for test can be remove / Nicolas
-
     map = game.add.tilemap('map'); //Load map with different layer, don't touch / Nicolas
     map.addTilesetImage('tileset_Interior', 'tiles');
     
-    layers = {                                  // Map all layers for player positionning
+    layers = { //Map all layers for player positionning
         floor: map.createLayer('floor'),
         stairs: map.createLayer('stairs'),
-        behind: map.createLayer('behind'),
         wall: map.createLayer('wall'),
+        windows: map.createLayer('windows'),
         carpet: map.createLayer('carpet'),
-        windows: map.createLayer('windows'),     
+        collision: map.createLayer('collision'),
         object: map.createLayer('object'),
+        collision2: map.createLayer('collision2'),
         object2: map.createLayer('object2'),
-        object3: map.createLayer('object3')
+        top: map.createLayer('top') //The sprite should be behind this layers. 
     }
-    map.setCollisionByExclusion([], true, layers.wall);         // Activata collicion with walls / Antoine
-    //layer.resizeWorld();
+    map.setCollisionByExclusion([], true, layers.wall); //Activate collision / Antoine
+    map.setCollisionByExclusion([], true, layers.collision); //Doesn't work
+    map.setCollisionByExclusion([], true, layers.collision2); //Doesn't work
 
     let waterConfig = {
         x: 100, y: 20, 
@@ -52,23 +51,16 @@ function create() {
     let timerConfig = {
         x: 500, y: 10,
         scale: 40,
-        duration:  105  //en secondes
+        duration:  105 //en secondes
     }
 
     waterBar = new EnergyBar(game, 'statusBar', 'dropOfWater', waterConfig);
     timer = new Timer(game, timerConfig);
     timer.start();
 
-    player = new Player(game, map, layers);                          // Spawn player after the map / Antoine
+    player = new Player(game, map, layers); //Spawn player after the map / Antoine
     
     
-}
-
-function gofull() { //Function fullscreen just for test / Nicolas
-    if (!game.scale.isFullScreen)
-    {
-        game.scale.startFullScreen(false);
-    }
 }
 
 function update() {
