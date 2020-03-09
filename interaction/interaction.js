63f2bc1efd2eca7fb6a4e4053f7611c08478435d
Nicolas Boizard <nicolas.boizard@isen.yncrea.fr>
2020-03-09 02:04:49 +0100

    Update interaction

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,15 +1,15 @@
 class interaction {
     constructor(){
-        this.object = []; //Array to store all objects.
-        this.electricity;
-        this.water;
+        this.object = []; //Array to store all objects. /Nicolas
+        this.electricity; //Value of instantaneous electricity consumption. /Nicolas
+        this.water; //Value of instantaneous water consumption. /Nicolas
         
-        this.object.push({tiledActivate: [1,2,3], tiledDesactivate: [5,8,12], type: "Electricity", value: 10}); //Example of object.
-
+        this.object.push({tiledActivate: [1,2,3], tiledDesactivate: [5,8,12], type: "electricity", value: 10, name: "light"}); //Example of object. /Nicolas
     }
 
     update(tiled){}
     get(type){}
 
     /* Dev bellow */
+    
 }
