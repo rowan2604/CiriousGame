be3337eb4d9b0a3f7eb682adde67968693b686f2
Nicolas Boizard <nicolas.boizard@isen.yncrea.fr>
2020-04-06 19:10:02 +0200

    Interaction finish 3/4

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,12 +1,52 @@
 class Interaction {
-    constructor(){
-        console.log("create");
+    constructor(data, waterBar, electricityBar){
+        this.data = data;
+        this.water = waterBar;
+        this.electricity = electricityBar;
+        this.number = this.data.objects.length;
+
+        this.coefElec = 1;
+        this.coefWater = 1;
+        this.currentElec = 0;
+        this.currentWater = 0;
     }
 
     interact(tiled, x, y){
-        console.log(tiled, x, y);
+        console.log(tiled, x, y); //debug
+        for(let i = 0; i < this.number; i++){
+            if(this.data.objects[i].aTileset == tiled){
+                if(this.data.objects[i].type == "Electric"){
+                    this.currentElec = this.currentElec - this.data.objects[i].consumption;
+                    //change tileset
+                }
+                else if(this.data.objects[i].type == "Water"){
+                    this.currentWater = this.currentWater - this.data.objects[i].consumption;
+                    //change tileset
+                }
+                break;
+            }
+            else if(this.data.objects[i].dTileset == tiled){
+                if(this.data.objects[i].type == "Electric"){
+                    this.currentElec += this.data.objects[i].consumption;
+                    //change tileset
+                }
+                else if(this.data.objects[i].type == "Water"){
+                    this.currentWater += this.data.objects[i].consumption;
+                    //change tileset
+                }
+                break;
+            }
+        }
     }
 
-    /* Dev bellow */
+    setCoef(type, value){
+        if(type == "Electric") {this.coefElec = value}
+        else if(type == "Water") {this.coefWater = value}
+    }
+
     
+    getValue(type){
+        if(type == "Electric") {return this.coefElec * this.currentElec}
+        else if(type == "Water") {return this.coefWater * this.coefWater}
+    }
 }
