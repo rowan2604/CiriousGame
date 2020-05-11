ac4bff2bb4414fa0402c4f44ac29ec5977c10370
rowan26 <rowan.hadjaz@isen.yncrea.fr>
2020-05-11 17:35:44 +0200

    men update

--- player/player.js
+++ player/player.js
@@ -42,6 +42,11 @@ class Player{
         // Animations
         this.currentDir = "down";
         this.initAnimations();
+
+        //Music players
+        this.music=this.game.add.audio('interupteur');
+        this.music1=this.game.add.audio('robinet');
+        this.music3=this.game.add.audio('tele');
     }
 
     initAnimations(){
